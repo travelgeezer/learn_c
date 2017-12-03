597b1965dadfa036885484419524c34fce05024e
travelgeezer <travelgeezer@icloud.com>
2017-12-03 16:55:35 +0800

    Format String

--- main.c
+++ main.c
@@ -5,5 +5,10 @@
 int main(void)
 {
   printf("Hello, world.\n");
+  printf("Goodbye, ");
+  printf("cruel world!\n");
+
+  printf("character: %c\ninterget: %d\nfloating point: %f\n", '}', 34, 3.14);
+
   return 0;
 }
