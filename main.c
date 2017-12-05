81e30e74c907a47076ad84a6d34071bfbc47f86d
travelgeezer <travelgeezer@icloud.com>
2017-12-05 11:16:59 +0800

    Modelo

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include <math.h>
 #include <stdio.h>
-
 /* main: generate some simple output */
 
 int ceiling(int x, int n);
-
+void increment(int x);
 
 int main(int argc, char *argv[])
 {
-  int result = ceiling(17, 4);
+  /*  int result = ceiling(17, 4); */
+  /* printf("%d\n", result); */
 
-  printf("%d\n", result);
+  printf("6 %% 16 = %d\n", 6 % 16);
+  printf("6 %% -16 = %d\n", 6 % -16);
+  printf("-6 %% 16 = %d\n", -6 % 16);
+  printf("-6 %% -16 = %d\n", -6 % -16);
+
+
   return 0;
 }
 
@@ -30,4 +35,10 @@ int ceiling(int x, int n) {
     {
       return resultInt;
     }
+}
+
+
+void increment(int x)
+{
+  x = x + 1;
 }
