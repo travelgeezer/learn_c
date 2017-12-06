e953e3334c4627eef6e36edbba2d89566eb11754
travelgeezer <travelgeezer@icloud.com>
2017-12-06 15:23:42 +0800

    Added enum

--- complex_struct.c
+++ complex_struct.c
@@ -1,18 +1,25 @@
 #include <math.h>
 
+enum coordinate_type { RECTANGULAR, POLAR };
+
 struct complex_struct {
+  enum coordinate_type t;
   double x, y;
 };
 
 struct complex_struct make_from_real_img(double x, double y)
 {
-  struct complex_struct z = { x, y };
+  struct complex_struct z;
+  z.t = RECTANGULAR;
+  z.x = x;
+  z.y = y;
   return z;
 }
 
 struct complex_struct make_from_mag_ang(double r, double A)
 {
   struct complex_struct z;
+  z.t = POLAR;
   z.x = r * cos(A);
   z.y = r * sin(A);
   return z;
