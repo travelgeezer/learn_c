bd5dbd96510435ef6a9741e20ee18797beb7f1f7
travelgeezer <travelgeezer@icloud.com>
2017-12-06 09:42:49 +0800

    Added gcd iteration version

--- main.c
+++ main.c
@@ -3,32 +3,60 @@
 /* main: generate some simple output */
 
 int gcd(int a, int b);
+int gcd_iteration(int a, int b);
+int is_gcd(int a, int b);
 int fab(int a);
 
 int main(int argc, char *argv[])
 {
 
-  printf("%d\n", gcd(16, 4));
+  printf("gcd %d\n", gcd(16, 4));
+  printf("gdc iteration %d\n", gcd_iteration(16, 4));
 
-  printf("%d\n", fab(20));
+  printf("gcd %d\n", gcd(16, 3));
+  printf("gdc iteration %d\n", gcd_iteration(16, 3));
+
 
   return 0;
 }
 
 int gcd(int a, int b) {
-  double da = a;
-  double db = b;
-
-  int iresult = a / b;
-  double dresult = da / db;
-
-  if (dresult - iresult == 0.00) {
+  if (is_gcd(a, b)) {
     return b;
   } else {
     return gcd(b, a % b);
   }
 }
 
+int gcd_iteration(int a, int b)
+{
+  int gcd = b;
+  while (!is_gcd(a, b))
+    {
+      int ta = a;
+      int tb = b;
+      a = tb;
+      b = ta % tb;
+      gcd = b;
+    }
+
+  return gcd;
+}
+
+int is_gcd(int a, int b)
+{
+  int ir = a / b;
+  double da = a;
+  double db = b;
+  double dr  = da / db;
+
+  double decimal = dr - ir;
+  if (decimal == 0.00) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 
 int fab(int a)
 {
